460a945213fb68cfcaebd473f3ae345fb8036b16
Leonardo Souza <leonardofs1910@gmail.com>
2024-10-29 09:06:25 -0300

    update code

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
